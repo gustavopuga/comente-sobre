06bfeb977509427203cfcb49e6e936fb04f523ad
Gustavo Fortunato Puga <gustavo.fortunatopuga@gmail.com>
2013-01-27 14:59:56 -0200

    Diretório com código java. Gerado a partir da prática de TDD.

---
+++ src/main/java/br/com/comente_sobre/domain/TalkAboutService.java
@@ -0,0 +1,11 @@
+package br.com.comente_sobre.domain;
+
+import br.com.comente_sobre.domain.model.Conversation;
+
+public class TalkAboutService {
+
+	public Conversation getConversation(String string) {
+		return new Conversation();
+	}
+
+}
---
+++ src/main/java/br/com/comente_sobre/domain/model/Conversation.java
@@ -0,0 +1,9 @@
+package br.com.comente_sobre.domain.model;
+
+public class Conversation {
+
+	public boolean isEmpty() {
+		return true;
+	}
+
+}
