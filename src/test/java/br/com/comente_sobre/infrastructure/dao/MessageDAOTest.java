2d8097bea1d035b00479d8d09ed4fd263880a533
Gustavo Fortunato Puga <gustavo.fortunatopuga@gmail.com>
2013-01-29 23:48:32 -0200

    Remoção de imports desnecessários da classe MessageDAOTest.

--- src/test/java/br/com/comente_sobre/infrastructure/dao/MessageDAOTest.java
+++ src/test/java/br/com/comente_sobre/infrastructure/dao/MessageDAOTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.hibernate.exception.ConstraintViolationException;
-import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
