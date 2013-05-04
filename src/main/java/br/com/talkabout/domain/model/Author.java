57212f128362fc775681568b9357832c96555683
Gustavo Fortunato Puga <gustavo.fortunatopuga@gmail.com>
2013-05-04 20:10:07 -0300

    Acrescentei um novo modelo agora para Autor. A ideia é retirar o nome e email do autor que esta provisóriamente dentro de mensagem.

---
+++ src/main/java/br/com/talkabout/domain/model/Author.java
@@ -0,0 +1,35 @@
+package br.com.talkabout.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "author")
+public class Author {
+
+	@Id
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "name")
+	private String name;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
